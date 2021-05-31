7a6e7756abe8d9f37b21c58d7d3d2c2f6f9cfc3b
fedebenelli <fedebenelli@outlook.com>
2021-05-31 01:56:08 +0000

    bitmap font on bar

--- .config/awesome/themes/xresources/theme.lua
+++ .config/awesome/themes/xresources/theme.lua
@@ -14,7 +14,7 @@ local themes_path = gfs.get_themes_dir()
 local theme = dofile(themes_path.."default/theme.lua")
 -- load vector assets' generators for this theme
 
-theme.font          = "monospace 8"
+theme.font          = "bitmap 8"
 
 theme.bg_normal     = xrdb.background
 theme.bg_focus      = xrdb.color12
