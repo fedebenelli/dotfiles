859749e6808e4627d539088ff949b2b61a58b27c
fedebenelli <fedebenelli@outlook.com>
2021-09-08 12:37:47 +0000

    another colorscheme I like

---
+++ .config/colors/vw/colors-themer.js
@@ -0,0 +1,20 @@
+module.exports.colors = {
+  dark: {
+    accent0: "#0F1624",
+    accent1: "#0c1dee",
+    accent2: "#0c21ec",
+    accent3: "#4051f4",
+    accent4: "#e118f2",
+    accent5: "#4897f5",
+    accent6: "#c5c6e7",
+    accent7: "#c5c6e7",
+    shade0:  "#0F1624",
+    shade1:  "#0c1dee",
+    shade2:  "#0c21ec",
+    shade3:  "#4051f4",
+    shade4:  "#e118f2",
+    shade5:  "#4897f5",
+    shade6:  "#21d1f3",
+    shade7:  "#c5c6e7"
+  },
+};
